435d41865ef70ba1abb41795ebc78e37d25c92be
arisgi <m1so.tama16@gmail.com>
2017-04-02 19:36:14 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import express  from 'express';
+import http     from 'http';
+import SocketIo from 'socket.io';
+
+const app    = express();
+const server = http.Server(app);
+const io     = new SocketIo(server);
+const PORT   = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log(`message: ${msg}`);
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(PORT, (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info(`listen: ${PORT}`);
+  }
+});
