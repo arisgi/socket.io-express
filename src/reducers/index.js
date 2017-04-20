177bce770c6c024d7d1177de9733cbe5089937f2
arisgi <m1so.tama16@gmail.com>
2017-04-20 23:22:56 +0900

    Add reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,24 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 const user = (state = { status: 'away' }, action) => {
   switch (action.type) {
+    case 'ENTER_ROOM':
+      return {
+        status: 'attend',
+        name: action.payload.name,
+      };
+    default:
+      return state;
+  }
+};
+
+const messages = (state = { list: [] }, action) => {
+  switch (action.type) {
+    case 'RECEIVE_MESSAGE':
+      return {
+        list: [...state.list, action.payload.message],
+      };
     default:
       return state;
   }
@@ -9,6 +26,8 @@ const user = (state = { status: 'away' }, action) => {
 
 const rootReducer = combineReducers({
   user,
+  messages,
+  form: formReducer,
 });
 
 export default rootReducer;
