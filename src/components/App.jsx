edea8b9182cd2b2d5acf1f40d480338ab724f2ad
arisgi <m1so.tama16@gmail.com>
2017-04-27 00:43:57 +0900

    Fix root component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 
-class App extends React.Component {
-  componentWillMount() {
-    if (this.props.user.status === 'away') {
-      this.props.router.replace('/');
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        hoge
-      </div>
-    );
-  }
+function App(props) {
+  return (
+    <div>
+      {props.children}
+    </div>
+  );
 }
 
 export default App;
