9a6711b0ad4a87ca1af1050541a51f27d4690a8e
arisgi <m1so.tama16@gmail.com>
2017-04-27 00:44:15 +0900

    Add routing

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import configureStore from './store/configureStore';
 import App from './containers/App';
+import Home from './containers/Home';
+import Room from './containers/Room';
 
 /* global document */
 
@@ -13,6 +15,8 @@ render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="room" component={Room} />
       </Route>
     </Router>
   </Provider>,
